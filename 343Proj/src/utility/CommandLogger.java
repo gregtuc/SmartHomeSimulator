8f0fe07e3fe0bfd35a0cea9d08b809ac7fef20ef
Greg <60011793+gregtuc@users.noreply.github.com>
2020-11-02 21:36:54 -0500

    Commit for #25 #27
    Created a CommandLogger class. It really has one method that accepts strings identifying the sender and the message to be printed. It prints to the "commandlog.txt" file and simultaneously prints to the scrollpane in Home.fxml.

---
+++ 343Proj/src/utility/CommandLogger.java
@@ -0,0 +1,28 @@
+package utility;
+
+import javafx.scene.control.TextArea;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CommandLogger {
+    private static volatile PermissionChecker instance = null;
+
+    public static void logCommand(String sender, String message, TextArea textarea) throws IOException {
+
+        String output = "["+sender+"] "+message;
+        //Write to output file.
+        try(FileWriter fw = new FileWriter("343Proj/src/commandlog.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.println(output);
+        } catch (IOException e) {
+            //exception handling left as an exercise for the reader
+        }
+        //Write to visual console.
+        textarea.setText(textarea.getText()+"\n"+output);
+    }
+}
